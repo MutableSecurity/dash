05c68ed524f9c7f27754e7c598dd8de143425305
George-Andrei Iosif <georgeiosif@live.com>
2022-08-25 16:49:22 +0300

    Computes overview statistics by querying Firebase
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { getLastMonthStatistics } from '../../utilities/user_data';
 
 export default function Overview() {
+    const [lastMonthStatistics, setLastMonthStatistics] = useState(null);
+
+    if (lastMonthStatistics == null) {
+        getLastMonthStatistics().then(result => {
+            setLastMonthStatistics(result);
+        });
+    }
+
     return <h1>Overview</h1>;
 }
--- src/utilities/user_data.js
+++ src/utilities/user_data.js
@@ -1,4 +1,12 @@
-import { child, get, getDatabase, ref } from 'firebase/database';
+import {
+    child,
+    get,
+    getDatabase,
+    orderByChild,
+    query,
+    ref,
+    startAt,
+} from 'firebase/database';
 
 import { plainToClass } from 'class-transformer';
 import { auth } from './auth';
@@ -36,6 +44,22 @@ class Settings {
     }
 }
 
+class MonthStastistics {
+    constructor(
+        reportsCount,
+        solutionsCounts,
+        availabilityPercentages,
+        failedTestsCounts,
+        passedTestsCounts
+    ) {
+        this.reportsCount = reportsCount;
+        this.solutionsCounts = solutionsCounts;
+        this.availabilityPercentages = availabilityPercentages;
+        this.failedTestsCounts = failedTestsCounts;
+        this.passedTestsCounts = passedTestsCounts;
+    }
+}
+
 const MockAccountDetails = new AccountDetails('', '', '', '');
 const MockAgentsConfiguration = new AgentsConfiguration(60);
 const MockReportingConfiguration = new ReportingConfiguration(60, 'daily');
@@ -44,6 +68,7 @@ export const MockSettings = new Settings(
     MockAgentsConfiguration,
     MockReportingConfiguration
 );
+export const MockMonthStatistics = new MonthStastistics(0, [], [], [], []);
 
 export function getUserSettings() {
     const userID = auth.currentUser.uid;
@@ -60,6 +85,73 @@ export function getUserSettings() {
             }
         })
         .catch(error => {
+            return null;
+        });
+}
+
+export function getLastMonthStatistics() {
+    const userID = auth.currentUser.uid;
+    const monthInSeconds = 31 * 24 * 60 * 60;
+    const startDate = Math.floor(Date.now() / 1000) - monthInSeconds;
+
+    var reportsRef = child(database, 'UserData/' + userID + '/reports');
+    var orderQuery = query(reportsRef, orderByChild('timestamp'));
+    var startsQuey = query(orderQuery, startAt(startDate, 'timestamp'));
+
+    return get(startsQuey)
+        .then(snapshot => {
+            if (snapshot.exists()) {
+                var reports = snapshot.val();
+                console.log(reports);
+
+                var reportsCount = 0;
+                var solutionsCounts = [];
+                var availabilityPercentages = [];
+                var failedTestsCounts = [];
+                var passedTestsCounts = [];
+
+                reports.forEach(report => {
+                    solutionsCounts.push(report.solutions.length);
+
+                    var failedTests = 0;
+                    var passedTests = 0;
+                    report.solutions.forEach(solution => {
+                        // console.log(solution.tests_results);
+                        if (solution.tests_results) {
+                            Object.values(solution.tests_results).forEach(
+                                value => {
+                                    if (value === true) {
+                                        passedTests += 1;
+                                    } else {
+                                        failedTests += 1;
+                                    }
+                                }
+                            );
+                        }
+                    });
+                    var availabilityPercentage =
+                        passedTests / (passedTests + failedTests);
+
+                    reportsCount += 1;
+
+                    availabilityPercentages.push(availabilityPercentage);
+                    failedTestsCounts.push(failedTests);
+                    passedTestsCounts.push(passedTests);
+                });
+
+                return new MonthStastistics(
+                    reportsCount,
+                    solutionsCounts,
+                    availabilityPercentages,
+                    failedTestsCounts,
+                    passedTestsCounts
+                );
+            } else {
+                return null;
+            }
+        })
+        .catch(error => {
+            console.log(error);
             return null;
         });
 }
