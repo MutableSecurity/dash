3b4049b5106da8a548de31d0d0c3b201880b2753
George-Andrei Iosif <georgeiosif@live.com>
2022-09-02 21:02:53 +0300

    Solves authentication bug on refresh
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -3,29 +3,44 @@ import {
     signInWithEmailAndPassword as firebaseSignInWithEmailAndPassword,
     signOut as firebaseSignOut,
 } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { app } from './firebase_controller';
+
+const LOGIN_TIMEOUT = 3000;
 
 export const auth = getAuth(app);
 
 var user = undefined;
 
-auth.onAuthStateChanged(changedUser => {
-    if (changedUser) {
-        user = changedUser;
-    } else {
-        user = undefined;
-    }
-});
-
 export function RequireActiveSession({ children }) {
-    user = auth.currentUser;
+    const [user, setUser] = useState(null);
+    const [isReady, setReady] = useState(false);
 
-    if (user) {
-        return children;
-    } else {
-        return <Navigate to="/" replace />;
+    useEffect(() => {
+        auth.onAuthStateChanged(changedUser => {
+            if (changedUser) {
+                var currentUser = getAuth(app).currentUser;
+
+                if (currentUser) {
+                    setUser(currentUser);
+                    setReady(true);
+                }
+            }
+        });
+
+        setTimeout(() => {
+            setReady(true);
+        }, LOGIN_TIMEOUT);
+    }, []);
+
+    if (isReady) {
+        if (user) {
+            return children;
+        } else {
+            return <Navigate to="/" replace />;
+        }
     }
 }
 
